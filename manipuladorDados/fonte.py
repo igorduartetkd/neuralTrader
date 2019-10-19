3a66fc2fa964ceaa8b79009342329455422ddfcb
igorduartetkd <igorduartetkd@gmail.com>
2019-10-19 17:45:45 -0300

    finalização do fluxo de criação dos dados de treinamento

--- manipuladorDados/fonte.py
+++ manipuladorDados/fonte.py
@@ -41,29 +41,3 @@ def getRegistrosSeparados(nome_ativo):
     return registros
 
 #getRegistrosSeparados("PETR4.SA")
-
-
-
-
-
-
-
-
-def remover_primeira_ultima_hora(registros):
-    for dia in registros:
-        data_inicio = dia[-1][0]
-        data_fim = dia[0][0]
-        print("Antes: {}".format(len(dia)))
-        dia = [x for x in dia[::-1] if not is_primeira_ultima_hora(data_inicio, data_fim, x[0])]
-        print("Depois: {}".format(len(dia)))
-
-def is_primeira_ultima_hora(data_inicio, data_fim, data):
-    data_inicio = datetime.datetime.fromtimestamp(data_inicio / 1000.0)
-    data_fim = datetime.datetime.fromtimestamp(data_fim / 1000.0)
-    data = datetime.datetime.fromtimestamp(data / 1000.0)
-    print(data)
-    if (data - data_inicio).seconds < 3600:
-        return True
-    if (data_fim - data).seconds < 3600:
-        return True
-    return False
--- manipuladorDados/prepararTreinamento.py
+++ manipuladorDados/prepararTreinamento.py
@@ -3,7 +3,7 @@ import fonte
 import datetime
 
 registros = []
-nome_arq_escrita_aumento = "dadosTreinamento.txt"
+nome_arq_escrita_aumento = "dadosTreinamento2.txt"
 arq = open(nome_arq_escrita_aumento, 'w')
 
 def preparar():
@@ -35,31 +35,56 @@ def is_primeira_ultima_hora(data_inicio, data_fim, data):
 def milis2data(milissegundos):
     return datetime.datetime.fromtimestamp(milissegundos / 1000.0)
 
+def armazenar_teste(arq, mini, maxi, teste, saida):
+    arq.write(str(mini) + ' ' + str(maxi) + '\n')
+    for i in teste:
+        arq.write(str(i) + ' ')
+    arq.write(str(saida) + '\n')
+
+
+def calcular_saida_esperada(media, ultimo, tipo=2):
+    saida_esperada = 0
+    if tipo == 1:
+        if media < ultimo:
+            saida_esperada = 1
+        elif media > ultimo:
+            saida_esperada = -1
+
+    elif tipo == 2:
+        if media * 1.001 < ultimo:
+            saida_esperada = 1
+        elif media * 0.999 > ultimo:
+            saida_esperada = -1
+
+    return saida_esperada
 
 def montar_linha_treinamento(dados):
     media_ultimos = calculadorEstatistico.media(dados[-5:-1])
-    saida_esperada = 0
-    if media_ultimos < dados[:-1]:
-        saida_esperada = 1
-    elif media_ultimos < dados[:-1]:
-        saida_esperada = -1
+    saida_esperada = calcular_saida_esperada(media_ultimos, dados[-1])
 
     mini, maxi, dados_normalizados = calculadorEstatistico.normalizar(dados[:-1])
     armazenar_teste(arq, mini, maxi, dados_normalizados, saida_esperada)
+
+
+def get_apenas_precos(registros):
+    novo_registro = []
+    for dia in registros:
+        dia = [x[1] for x in dia]
+        novo_registro.append(dia)
+    return novo_registro
 
 
 def criar_treinamento(janela_size):
     global registros
     preparar()
     registros = remover_primeira_ultima_hora(registros)
+    registros = get_apenas_precos(registros)
+
+    for dia in registros:
+        for i in range(len(dia) - janela_size - 1):
+            montar_linha_treinamento(dia[i:i + janela_size + 1])
 
 
-
-def armazenar_teste(arq, mini, maxi, teste, saida):
-    arq.write(str(mini) + ' ' + str(maxi) + '\n')
-    for i in teste:
-        arq.write(str(i) + ' ')
-    arq.write(str(saida) + '\n')
 
 criar_treinamento(20)
 
